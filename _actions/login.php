eff2199428b64145ba91278b5ffacbe93dc7b0cb
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-24 22:33:55 +0630

    cookie and session testing

---
+++ _actions/login.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if ($email === "kyawminhtway.@gmail.com" and $password === 'admin123') {
+    $_SESSION['user'] = ['username' => 'Kyaw Min Htway'];
+    header('location: ../profile.php');
+}else {
+    header('location: ../index.php?incorrect=1');
+}
