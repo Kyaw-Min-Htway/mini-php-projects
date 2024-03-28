c5f29ee8826800ddd3d40cdd8374bd2dd73aba28
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-28 21:33:23 +0630

    Auth and Http

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        .wrap {
+            width: 100%;
+            max-width: 400px;
+            margin: 40px auto;
+        }
+    </style>
+</head>
+<body class="text-center">
+    <div class="wrap">
+        <h1 class="h3 mb-3">Register</h1>
+    </div>
+</body>
+</html>
